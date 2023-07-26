fb900dfec892030a6007d0be18400dcff9edfc13
GibbsQin <shenghua.qin@outlook.com>
2023-07-26 13:18:38 +0800

    libavformat/dashdec:support selecting dash period

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -150,6 +150,7 @@ typedef struct DASHContext {
     AVDictionary *avio_opts;
     int max_url_size;
     int refresh_advance_manifest;
+    int prefer_period_index;
 } DASHContext;
 
 static int ishttp(char *url)
@@ -1126,6 +1127,8 @@ static int parse_manifest(AVFormatContext *s, const char *url, AVIOContext *in)
     char *val  = NULL;
     uint32_t period_duration_sec = 0;
     uint32_t period_start_sec = 0;
+    int cur_period_idx = 0;
+    int period_need_update = 0;
 
     if (!in) {
         close_in = 1;
@@ -1233,7 +1236,12 @@ static int parse_manifest(AVFormatContext *s, const char *url, AVIOContext *in)
                     attr = attr->next;
                     xmlFree(val);
                 }
-                if ((period_duration_sec) >= (c->period_duration)) {
+                if (c->prefer_period_index > 0) {
+                    period_need_update = (c->prefer_period_index == cur_period_idx++) ? 0 : 1;
+                } else {
+                    period_need_update = (period_duration_sec) >= (c->period_duration);
+                }
+                if (period_need_update) {
                     period_node = node;
                     c->period_duration = period_duration_sec;
                     c->period_start = period_start_sec;
@@ -1242,6 +1250,9 @@ static int parse_manifest(AVFormatContext *s, const char *url, AVIOContext *in)
                 }
             }
             node = xmlNextElementSibling(node);
+        if (c->prefer_period_index > 0 && cur_period_idx > 1) {
+            av_log(s, AV_LOG_INFO, "select the period index %d\n", c->prefer_period_index);
+            av_dict_set_int(&s->metadata, "IJK-PLAYLIST-INFO", cur_period_idx * 10000 + c->prefer_period_index, 0);
         }
         if (!period_node) {
             av_log(s, AV_LOG_ERROR, "Unable to parse '%s' - missing Period node\n", url);
@@ -2184,6 +2195,8 @@ static const AVOption dash_options[] = {
         INT_MIN, INT_MAX, FLAGS},
     {"refresh-advance-manifest", "refresh manifest before download the advance segment",
         OFFSET(refresh_advance_manifest), AV_OPT_TYPE_INT, {.i64 = 1}, 0, 1, FLAGS},
+    {"prefer-period-index", "select the period equal the index",
+        OFFSET(prefer_period_index), AV_OPT_TYPE_INT, {.i64 = 0}, INT_MIN, INT_MAX, FLAGS},
     {NULL}
 };
 
