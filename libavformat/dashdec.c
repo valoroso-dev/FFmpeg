e81c2098aa21660b3bc490aba14fd8e88bf63bc4
GibbsQin <shenghua.qin@outlook.com>
2023-07-28 09:21:11 +0800

    libavformat/dashdec:support dash seeking

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -1789,10 +1789,11 @@ static int reopen_demux_for_component(AVFormatContext *s, struct representation 
     }
     if (c->is_live) {
         ffio_init_context(&pls->pb, avio_ctx_buffer , INITIAL_BUFFER_SIZE, 0, pls, read_data, NULL, NULL);
+        pls->pb.seekable = 0;
     } else {
         ffio_init_context(&pls->pb, avio_ctx_buffer , INITIAL_BUFFER_SIZE, 0, pls, read_data, NULL, seek_data);
+        pls->pb.seekable = pls->n_fragments == 1 ? AVIO_SEEKABLE_NORMAL : 0;
     }
-    pls->pb.seekable = 0;
 
     if ((ret = ff_copy_whiteblacklists(pls->ctx, s)) < 0)
         goto fail;
